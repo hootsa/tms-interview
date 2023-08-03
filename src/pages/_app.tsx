a6a5c84cf3f218766e0d9619aaee07132ef89d3d
amir <amir@truesocialmarketing.com>
2023-08-02 17:00:33 -0700

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
 import Layout from "@/components/layout/Layout";
 import HomePage from "@/sections/homePage";
 
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
+
 export default function Home() {
   return (
     <Layout>
